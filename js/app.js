8468422820a8f2c78216e5d92354eef1d13ca9c7
Vedant <mainvedant.vns123@gmail.com>
2018-09-08 12:18:21 +0530

    df

--- js/app.js
+++ js/app.js
@@ -47,24 +47,129 @@ myApp.config(function ($routeProvider) {
     $routeProvider
     
     .when('/', {
-        templateUrl: 'pages/home.html',
+        template: 'loading',
         controller: 'homeController'
     }) .when('/login', {
       templateUrl: 'pages/login.html',
       controller: 'loginController'
   })
-
+  .when('/dashboard', {
+    templateUrl: 'pages/dash.html',
+    controller: 'dashController'
+})
    
 });
 
 myApp.controller('homeController', ['$scope', '$http', '$location','$window','$rootScope','$route','fileUpload', function($scope,$http,$location,$window,$rootScope,$route,fileUpload){
+  
+  $window.location.href = 'https://vedantjain0087.github.io/ipec_temp/'; 
+  
+  // particlesJS.load('particles-js', 'assets/particles.json', function() {
+  //   console.log('callback - particles.js config loaded');
+  // });
 
-var socket = io.connect("http://localhost:3000");
-socket.emit("send_message","Hello world");
+
+  // var typed = new Typed('#typed', {
+  //   stringsElement: '#typed-strings',
+	// loop: true,
+	//  typeSpeed: 30,
+	//  backDelay: 900,
+	//    backSpeed: 30,
+  // });
+
+  // $(function() {
+  //   // Smooth Scrolling
+  //   $('a[href*="#"]:not([href="#"])').click(function() {
+  //     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //       var target = $(this.hash);
+  //       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //       if (target.length) {
+  //         $('html, body').animate({
+  //           scrollTop: target.offset().top
+  //         }, 1000);
+  //         return false;
+  //       }
+  //     }
+  //   });
+  // });
+  
+  var socket = io.connect("https://ipec.herokuapp.com");
+
+
+//ChAt Scripts
+
+$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+
+$("#profile-img").click(function() {
+	$("#status-options").toggleClass("active");
+});
+
+$(".expand-button").click(function() {
+  $("#profile").toggleClass("expanded");
+	$("#contacts").toggleClass("expanded");
+});
+
+$("#status-options ul li").click(function() {
+	$("#profile-img").removeClass();
+	$("#status-online").removeClass("active");
+	$("#status-away").removeClass("active");
+	$("#status-busy").removeClass("active");
+	$("#status-offline").removeClass("active");
+	$(this).addClass("active");
+	
+	if($("#status-online").hasClass("active")) {
+		$("#profile-img").addClass("online");
+	} else if ($("#status-away").hasClass("active")) {
+		$("#profile-img").addClass("away");
+	} else if ($("#status-busy").hasClass("active")) {
+		$("#profile-img").addClass("busy");
+	} else if ($("#status-offline").hasClass("active")) {
+		$("#profile-img").addClass("offline");
+	} else {
+		$("#profile-img").removeClass();
+	};
+	
+	$("#status-options").removeClass("active");
+});
+
+function newMessage() {
+	message = $(".message-input input").val();
+	if($.trim(message) == '') {
+		return false;
+  }
+  socket.emit("send_message",{"msg":message,"username":$window.sessionStorage.username});
+	$('<li class="replies"><img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
+	$('.message-input input').val(null);
+	$('.contact.active .preview').html('<span>You: </span>' + message);
+	$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+};
+
+$('.submit').click(function() {
+  newMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    newMessage();
+    return false;
+  }
+});
+
+//End of chat script
+
+
+
+
 
 socket.on("new_message",function(data){
-console.log(data);
 
+console.log(data);
+if(data.username != $window.sessionStorage.username){
+$('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + data.msg + '</p></li>').appendTo($('.messages ul'));
+
+	// $('.contact.active .preview').html('<span>You: </span>' + message);
+	$(".messages").animate({ scrollTop: $(document).height() }, "fast");
+}
 });
 
 }]);
@@ -82,7 +187,10 @@ console.log($scope.password);
     //data:{'data': data}
 })
 .then(function(response) {
-console.log(response.status);
+if(response.status == 200){
+  $window.sessionStorage.setItem("username",$scope.username );
+  $window.location.href = '#';
+}
       
 
  });
@@ -91,4 +199,8 @@ console.log(response.status);
   
   }]);
   
+  myApp.controller('dashController', ['$scope', '$http', '$location','$window','$rootScope','$route','fileUpload', function($scope,$http,$location,$window,$rootScope,$route,fileUpload){
 
+ 
+     
+ }]);
